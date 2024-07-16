dd6d6f383766836bd674a4cd89dd245aefb655dd
yr0202 <yooram0202@naver.com>
2024-07-16 18:48:24 +0900

    0716

--- ch2/src/main/java/com/fastcampus/ch2/Hello.java
+++ ch2/src/main/java/com/fastcampus/ch2/Hello.java
@@ -5,8 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Hello {
+    int iv = 10;
+    static int cv = 20;
+
     @RequestMapping("/hello")
-    public void main(String[] args) {
+    private void main() {
         System.out.println("Hello World yuram");
+        System.out.println("private hello world");
+        System.out.println(iv);
+        System.out.println(cv);
+    }
+    public static void main2(){
+        System.out.println(cv);
+//        System.out.println(iv); //에러
     }
 }
---
+++ ch2/src/main/java/com/fastcampus/ch2/Main.java
@@ -0,0 +1,21 @@
+package com.fastcampus.ch2;
+
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+//        Hello hello = new Hello();
+//        hello.main(); // Hello 클래스의 main 메서드가 private이라서 외부호출 불가
+
+        //Reflection API를 사용 - 클래스 정보를 얻고 다룰 수 있는 강력한 기능제공
+        // java.lang.reflect패키지를 제공
+
+        // Hello클래스의 Class객체를 얻어온다
+        Class helloClass = Class.forName("com.fastcampus.ch2.Hello");
+        Hello hello = (Hello) helloClass.newInstance(); // Class 객체가 가진 정보로 객체 생성
+        Method main = helloClass.getDeclaredMethod("main");
+        main.setAccessible(true); // private인 main을 호출 가능하게 함
+        main.invoke(hello);
+    }
+
+}
